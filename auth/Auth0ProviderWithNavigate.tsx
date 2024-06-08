119d62b58614639dc52cb8c5513a518db58ce366
daniel-budai <danielbudai@tutanota.com>
2024-06-08 08:14:55 +0200

    Validate Access Token

--- auth/Auth0ProviderWithNavigate.tsx
+++ auth/Auth0ProviderWithNavigate.tsx
@@ -10,8 +10,9 @@ const Auth0ProviderWithNavigate = ({ children }: props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI;
+  const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
-  if (!domain || !clientId || !redirectUri) {
+  if (!domain || !clientId || !redirectUri || !audience) {
     throw new Error("unable to inizialize Auth");
   }
 
@@ -25,6 +26,7 @@ const Auth0ProviderWithNavigate = ({ children }: props) => {
       clientId={clientId}
       authorizationParams={{
         redirect_uri: redirectUri,
+        audience,
       }}
       onRedirectCallback={onRedirectCallback}
     >
