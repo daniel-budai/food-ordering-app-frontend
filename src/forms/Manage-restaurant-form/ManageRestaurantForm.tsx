a5649664609c9ca47e298cadf03a7eaaca464901
daniel-budai <danielbudai@tutanota.com>
2024-06-19 07:10:16 +0200

    continue on checkbox, cuisinesection

--- src/forms/Manage-restaurant-form/ManageRestaurantForm.tsx
+++ src/forms/Manage-restaurant-form/ManageRestaurantForm.tsx
@@ -3,6 +3,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
 import DetailsSection from "./DetailsSection";
+import { Separator } from "@/components/ui/separator";
 
 const formSchema = z.object({
   restaurantName: z.string({
@@ -57,6 +58,7 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
         className="space-y-8 bg-gray-50 p-10 rounded-lg"
       >
         <DetailsSection />
+        <Separator />
       </form>
     </Form>
   );
