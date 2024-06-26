9a315fce2031a222bb7cdbab813c6497fd910063
daniel-budai <danielbudai@tutanota.com>
2024-06-26 13:23:58 +0200

    stripe create session

--- src/components/CheckoutButton.tsx
+++ src/components/CheckoutButton.tsx
@@ -11,9 +11,10 @@ import { useGetMyUser } from "@/api/MyUserApi";
 type Props = {
   onCheckout: (userFormData: UserFormData) => void;
   disabled: boolean;
+  isLoading: boolean;
 };
 
-const CheckoutButton = ({ onCheckout, disabled }: Props) => {
+const CheckoutButton = ({ onCheckout, disabled, isLoading }: Props) => {
   const {
     isAuthenticated,
     isLoading: isAuthLoading,
@@ -36,7 +37,7 @@ const CheckoutButton = ({ onCheckout, disabled }: Props) => {
     );
   }
 
-  if (isAuthLoading) {
+  if (isAuthLoading || !currentUser || isLoading) {
     return <LoadingButton />;
   }
 
