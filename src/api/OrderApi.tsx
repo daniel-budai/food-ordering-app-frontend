1b21f9b4771cf7391ef836d99492bc66ae4c67e1
daniel-budai <danielbudai@tutanota.com>
2024-08-05 10:30:19 +0200

    polling

--- src/api/OrderApi.tsx
+++ src/api/OrderApi.tsx
@@ -22,7 +22,8 @@ export const useGetMyOrders = () => {
   };
   const { data: orders, isLoading } = useQuery(
     "fetchMyOrders",
-    getMyOrdersRequest
+    getMyOrdersRequest,
+    { refetchInterval: 5000 }
   );
   return { orders, isLoading };
 };
